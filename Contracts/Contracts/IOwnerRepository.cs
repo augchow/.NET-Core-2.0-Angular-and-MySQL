31139368c30ca55aaccf8e6069f8e9df4e9f9011
Augustine Chow <aug_chow00@hotmail.com>
2018-12-13 16:09:01 +0800

    Completed GET request

--- Contracts/Contracts/IOwnerRepository.cs
+++ Contracts/Contracts/IOwnerRepository.cs
@@ -1,9 +1,14 @@
-﻿using Entities.Models;
+﻿using Entities.ExtendedModels;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Contracts
 {
     public interface IOwnerRepository : IRepositoryBase<Owner>
     {
-
-    }
+        IEnumerable<Owner> GetAllOwners();
+        Owner GetOwnerById(Guid ownerId);
+        OwnerExtended GetOwnerWithDetails(Guid ownerId);
+    }  
 }
